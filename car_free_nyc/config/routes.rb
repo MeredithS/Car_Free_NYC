b9368d25cc0cf3ab801feb775343f74f6c6dc6d5
Meredith Salguero <salguero.meredith@gmail.com>
2016-03-31 15:26:36 -0400

    routes defined

--- car_free_nyc/config/routes.rb
+++ car_free_nyc/config/routes.rb
@@ -3,7 +3,13 @@ Rails.application.routes.draw do
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  # root 'welcome#index'
+   root 'welcome#index'
+
+  shallow do 
+   resources :users, only: [:index, :new, :create] do
+    resources :social_handles, only: [:index, :create, :new]
+    end
+  end
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
