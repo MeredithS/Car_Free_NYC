2a996191b7fc5cc6a4d94bad9e8de5235251959d
Meredith Salguero <salguero.meredith@gmail.com>
2016-04-01 16:45:55 -0400

    add ability to create users and social handles also redirects to pledges page

--- car_free_nyc/app/controllers/users_controller.rb
+++ car_free_nyc/app/controllers/users_controller.rb
@@ -1,5 +1,6 @@
 class UsersController < ApplicationController
 	def index
+		@users = User.all
 	end
 
 	def new
@@ -7,6 +8,22 @@ class UsersController < ApplicationController
 	end
 
 	def create
+		@user = User.new user_params
+		@user.date = DateTime.now
+		if @user.save
+			@user.social_handles.create social_params
+			flash[:notice] = "Thank you " + @user.f_name + " for your pledge!"
+			redirect_to pledge_confirmation_url
+		end
 	end
 	
 end
+
+private
+	def user_params
+		params.require(:user).permit(:f_name, :l_name, :e_mail, :commitment, :org, :date, :share_info?)
+	end
+
+	def social_params
+		params.require(:social_handle).permit(:twitter, :facebook, :instagram)
+	end
