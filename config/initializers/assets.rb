b269e95c8ffebc6b1730105130f3983a7cad1ab4
davidhickey <davidhickey423@gmail.com>
2016-04-06 18:11:58 -0400

    changes to stylesheet structure

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -4,6 +4,7 @@
 Rails.application.config.assets.version = '1.0'
 Rails.application.config.assets.precompile += %w( sponsors.css )
 Rails.application.config.assets.precompile += %w( new_user.css )
+Rails.application.config.assets.precompile += %w( root.css )
 
 # Add additional assets to the asset load path
 # Rails.application.config.assets.paths << Emoji.images_path
