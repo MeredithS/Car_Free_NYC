a7b044150aa19e120e811405b15b168e2a5da385
Meredith Salguero <salguero.meredith@gmail.com>
2016-04-07 12:36:24 -0400

    comments out seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,9 +5,9 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
-u1 = User.create({f_name: 'Meredith',l_name: 'Salguero',e_mail: 'Mer@yahoo.com',commitment:'I pledge to fly on April 22.',org: 'Foundation Awesome',share_info?: false})
+# u1 = User.create({f_name: 'Meredith',l_name: 'Salguero',e_mail: 'Mer@yahoo.com',commitment:'I pledge to fly on April 22.',org: 'Foundation Awesome',share_info?: false})
 
-u2 = User.create({f_name: 'David',l_name:'Hickey',e_mail: 'DavidIsKool@gmail.com',commitment: 'I pledge to teleport on Earth Day.', org: 'David and Goliath',share_info?: true})
+# u2 = User.create({f_name: 'David',l_name:'Hickey',e_mail: 'DavidIsKool@gmail.com',commitment: 'I pledge to teleport on Earth Day.', org: 'David and Goliath',share_info?: true})
 
-s1 = SocialHandle.create({twitter: '@davidVgoliath', instagram: '@bob', user_id: 2})
+# s1 = SocialHandle.create({twitter: '@davidVgoliath', instagram: '@bob', user_id: 2})
 
